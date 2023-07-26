744d56f2f247903e4f17b68dcfa327e4324404c3
Sukruti Mallesh <sukrutimallesh@gmail.com>
2023-07-26 12:21:31 -0700

    auto sign in and routing

--- app.js
+++ app.js
@@ -11,38 +11,59 @@ app.use(bodyParser.json());
 // Define your routes
 app.post('/automateZybooks', async (req, res) => {
   try {
-    const targetUrl = 'https://learn.zybooks.com/zybook/SDSUCS150Fall2022/chapter/2/section/1'; // URL of the specific Zybooks page you want to interact with
-    const targetKeywords = ['Start']; // Keywords of elements you want to click
+    // Extract username and password from the request JSON payload
+    const { username, password, links, keywords } = req.body;
 
     // Initialize the web driver (make sure you have the appropriate driver for Chrome)
     const driver = await new Builder().forBrowser('chrome').build();
 
-    // Navigate to the target page
-    await driver.get(targetUrl);
+    // Navigate to the Zybooks sign-in page
+    await driver.get('https://learn.zybooks.com/signin');
 
-    // Wait for the page to load (add appropriate wait times or use WebDriver's ExpectedConditions for better handling)
+    // Wait for the sign-in page to load (add appropriate wait times or use WebDriver's ExpectedConditions for better handling)
     await driver.sleep(5000);
 
-    // Look for keywords in the page content
-    for (const keyword of targetKeywords) {
-      const pageContent = await driver.getPageSource();
-      if (pageContent.includes(keyword)) {
-        // Find the element and click on it
-        const element = await driver.findElement(By.xpath(`//*[contains(text(), '${keyword}')]`));
-        await element.click();
+    // Find the username/email input field and input the username
+    const usernameField = await driver.findElement(By.css('input[type="email"]'));
+    await usernameField.sendKeys(username);
 
-        // Wait for a moment to simulate human interaction
-        await driver.sleep(2000);
+    // Find the password input field and input the password
+    const passwordField = await driver.findElement(By.css('input[type="password"]'));
+    await passwordField.sendKeys(password);
+
+    // Find the sign-in button and click it
+    const signInButton = await driver.findElement(By.css('button.signin-button'));
+    await signInButton.click();
+
+    // Wait for authentication to complete (add appropriate waits based on the website's behavior)
+    await driver.sleep(5000);
+
+    // Proceed with your desired interactions on the authenticated Zybooks page
+
+    // The WebDriver will continue running after signing in, allowing you to interact with the website.
+
+    // Loop through each link and search for the keyword
+    for (const link of links) {
+        await driver.get(link); // Navigate to the next link
+  
+        // Wait for the page to load (add appropriate wait times or use WebDriver's ExpectedConditions for better handling)
+        await driver.sleep(5000);
+  
+        // Search for the "Start" keyword and click on it
+        for (const keyword of keywords) {
+          const pageContent = await driver.getPageSource();
+          if (pageContent.includes(keyword)) {
+            const element = await driver.findElement(By.xpath(`//span[contains(text(), '${keyword}')]`));
+            await element.click();
+            await driver.sleep(2000); // Wait for a moment to simulate human interaction
+          }
+        }
       }
-    }
 
-    // Close the browser
-    await driver.quit();
-
-    res.status(200).json({ message: 'Automation completed successfully.' });
+    res.status(200).json({ message: 'Authentication and automation completed successfully.' });
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({ error: 'Something went wrong.' });
+    res.status(500).json({ error: 'Something went wrong during authentication and automation.' });
   }
 });
 
